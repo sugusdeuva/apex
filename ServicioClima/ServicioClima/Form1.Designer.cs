a09091df546b98e18f3a094ef1e84a6563cd5af8
Seba galli <sebastiangalli20@gmail.com>
2021-09-30 17:51:08 -0300

    SERVICIO CLIMA V.2

--- ServicioClima/ServicioClima/Form1.Designer.cs
+++ ServicioClima/ServicioClima/Form1.Designer.cs
@@ -40,6 +40,10 @@ namespace ServicioClima
             this.pictureBox8 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -52,66 +56,82 @@ namespace ServicioClima
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Location = new System.Drawing.Point(35, 313);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(97, 92);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
             // 
             // pictureBox2
             // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.Location = new System.Drawing.Point(187, 313);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(100, 92);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox3.Location = new System.Drawing.Point(351, 313);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(100, 92);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 2;
             this.pictureBox3.TabStop = false;
             // 
             // pictureBox4
             // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox4.Location = new System.Drawing.Point(827, 313);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(100, 92);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 3;
             this.pictureBox4.TabStop = false;
             // 
             // pictureBox5
             // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox5.Location = new System.Drawing.Point(510, 313);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(100, 92);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox5.TabIndex = 3;
             this.pictureBox5.TabStop = false;
             // 
             // pictureBox6
             // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox6.Location = new System.Drawing.Point(669, 313);
             this.pictureBox6.Name = "pictureBox6";
             this.pictureBox6.Size = new System.Drawing.Size(100, 92);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox6.TabIndex = 4;
             this.pictureBox6.TabStop = false;
             // 
             // pictureBox7
             // 
+            this.pictureBox7.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox7.Location = new System.Drawing.Point(979, 313);
             this.pictureBox7.Name = "pictureBox7";
             this.pictureBox7.Size = new System.Drawing.Size(100, 92);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox7.TabIndex = 5;
             this.pictureBox7.TabStop = false;
             // 
             // pictureBox8
             // 
+            this.pictureBox8.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox8.Location = new System.Drawing.Point(1124, 313);
             this.pictureBox8.Name = "pictureBox8";
             this.pictureBox8.Size = new System.Drawing.Size(100, 92);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox8.TabIndex = 6;
             this.pictureBox8.TabStop = false;
             // 
@@ -139,12 +159,62 @@ namespace ServicioClima
             this.label2.Size = new System.Drawing.Size(0, 29);
             this.label2.TabIndex = 8;
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label3.Location = new System.Drawing.Point(30, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 29);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Time:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Image = global::ServicioClima.Properties.Resources.c156b4270aea292b9b335dd463ea17eb_icono_de_planeta_tierra_icono_de_tierra;
+            this.button1.Location = new System.Drawing.Point(530, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 73);
+            this.button1.TabIndex = 10;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label4.Location = new System.Drawing.Point(994, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 29);
+            this.label4.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Roboto", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label5.Location = new System.Drawing.Point(1000, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 29);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "TEMP";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(1258, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox8);
@@ -183,6 +253,10 @@ namespace ServicioClima
         private System.Windows.Forms.PictureBox pictureBox8;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- ServicioClima/ServicioClima/Form1.cs
+++ ServicioClima/ServicioClima/Form1.cs
@@ -19,6 +19,13 @@ namespace ServicioClima
         {
             InitializeComponent();
         }
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dateTime;
+        }
         private void pictureBox1_Click(object sender, EventArgs e)
         {
             string sURL;
@@ -48,6 +55,15 @@ namespace ServicioClima
 
             json = JObject.Parse(sLinePrev);
 
+            pictureBox1.Image = Image.FromFile("iconos/" + json["daily"]["data"][0]["icon"].ToString() + ".png");
+            pictureBox2.Image = Image.FromFile("iconos/" + json["daily"]["data"][1]["icon"].ToString() + ".png");
+            pictureBox3.Image = Image.FromFile("iconos/" + json["daily"]["data"][2]["icon"].ToString() + ".png");
+            pictureBox4.Image = Image.FromFile("iconos/" + json["daily"]["data"][3]["icon"].ToString() + ".png");
+            pictureBox5.Image = Image.FromFile("iconos/" + json["daily"]["data"][4]["icon"].ToString() + ".png");
+            pictureBox6.Image = Image.FromFile("iconos/" + json["daily"]["data"][5]["icon"].ToString() + ".png");
+            pictureBox7.Image = Image.FromFile("iconos/" + json["daily"]["data"][6]["icon"].ToString() + ".png");
+            pictureBox8.Image = Image.FromFile("iconos/" + json["daily"]["data"][7]["icon"].ToString() + ".png");
+
             MessageBox.Show(json["daily"]["data"][0]["icon"] + ".jpg");
             MessageBox.Show(json["daily"]["data"][1]["icon"] + ".jpg");
             MessageBox.Show(json["daily"]["data"][2]["icon"] + ".jpg");
@@ -69,5 +85,60 @@ namespace ServicioClima
         {
 
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sURL;
+            WebRequest wrGETURL;
+            JObject json;
+            Stream objStream;
+
+            sURL = "https://api.darksky.net/forecast/b4497c8a6b18bf75776b7fd997f2b90a/-34.6119666,-58.4030375?lang=es&units=ca";
+            wrGETURL = WebRequest.Create(sURL);
+            objStream = wrGETURL.GetResponse().GetResponseStream();
+            StreamReader objReader = new StreamReader(objStream);
+
+            string sLine = "";
+            string sLinePrev = "";
+            int i = 0;
+
+            while (sLine != null)
+            {
+                i++;
+                sLinePrev = sLine;
+                sLine = objReader.ReadLine();
+                //if (sLine != null)
+                //    MessageBox.Show(sLine);                    
+
+            }
+            //MessageBox.Show(sLinePrev);
+
+            json = JObject.Parse(sLinePrev);
+
+            pictureBox1.Image = Image.FromFile("iconos/" + json["daily"]["data"][0]["icon"].ToString() + ".png");
+            pictureBox2.Image = Image.FromFile("iconos/" + json["daily"]["data"][1]["icon"].ToString() + ".png");
+            pictureBox3.Image = Image.FromFile("iconos/" + json["daily"]["data"][2]["icon"].ToString() + ".png");
+            pictureBox4.Image = Image.FromFile("iconos/" + json["daily"]["data"][3]["icon"].ToString() + ".png");
+            pictureBox5.Image = Image.FromFile("iconos/" + json["daily"]["data"][4]["icon"].ToString() + ".png");
+            pictureBox6.Image = Image.FromFile("iconos/" + json["daily"]["data"][5]["icon"].ToString() + ".png");
+            pictureBox7.Image = Image.FromFile("iconos/" + json["daily"]["data"][6]["icon"].ToString() + ".png");
+            pictureBox8.Image = Image.FromFile("iconos/" + json["daily"]["data"][7]["icon"].ToString() + ".png");
+            
+            DateTime d = UnixTimeStampToDateTime(Convert.ToDouble(json["currently"]["time"]));
+            string s = d.ToLongDateString();
+           
+            /*MessageBox.Show(json["daily"]["data"][0]["icon"] + ".jpg");
+            MessageBox.Show(json["daily"]["data"][1]["icon"] + ".jpg");
+            MessageBox.Show(json["daily"]["data"][2]["icon"] + ".jpg");
+            */
+            Console.ReadLine();
+        }
+       
+        
     }
 }
